8cf1ba981726aaf70d34846b20f5bb461968ff90
junho-yu <121595773+junho-yu@users.noreply.github.com>
2023-01-10 15:03:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -20,40 +20,41 @@ login.addEventListener('click', () => {
 
 })
 function decrypt(pwd) {
-    var str = "5ef076786860e1cdb8e892ba279ae7ac56b5bec9e8c8554705ac72142d7d7610a0bfa2af71cf0a16b804d90cd6";
-    if (pwd == null || pwd.length <= 0) {
-        alert('비밀번호를 입력하세요.');
-        return;
-    }
-    var prand = "";
-    for (var i = 0; i < pwd.length; i++) {
-        prand += pwd.charCodeAt(i).toString();
-    }
-    var sPos = Math.floor(prand.length / 5);
-    var mult = parseInt(prand.charAt(sPos) + prand.charAt(sPos * 2) + prand.charAt(sPos * 3) + prand.charAt(sPos * 4) + prand.charAt(sPos * 5));
-    var incr = Math.round(pwd.length / 2);
-    var modu = Math.pow(2, 31) - 1;
-    var salt = parseInt(str.substring(str.length - 8, str.length), 16);
-    str = str.substring(0, str.length - 8);
-    prand += salt;
-    while (prand.length > 10) {
-        prand = (parseInt(prand.substring(0, 10)) + parseInt(prand.substring(10, prand.length))).toString();
-    }
-    prand = (mult * prand + incr) % modu;
-    var enc_chr = "";
-    var enc_str = "";
-    for (var i = 0; i < str.length; i += 2) {
-        enc_chr = parseInt(parseInt(str.substring(i, i + 2), 16) ^ Math.floor((prand / modu) * 255));
-        enc_str += String.fromCharCode(enc_chr);
-        prand = (mult * prand + incr) % modu;
-    }
-    if (!enc_str.match(/\.html/g)) {
-        alert("틀렸어...잘 생각해봐");
-    }
-    else {
-        window.location.href = enc_str;
-    }
+	var str="fe248ed43cb10766534748a0e91b580efd7bd4e203711311034185160ac8409442fa21a129c30c6d00a8bf73";
+	if(pwd == null || pwd.length <= 0) {
+		alert('비밀번호를 입력하세요.');
+		return;
+	}
+	var prand = "";
+	for(var i=0; i<pwd.length; i++) {
+		prand += pwd.charCodeAt(i).toString();
+	}
+	var sPos = Math.floor(prand.length / 5);
+	var mult = parseInt(prand.charAt(sPos) + prand.charAt(sPos*2) + prand.charAt(sPos*3) + prand.charAt(sPos*4) + prand.charAt(sPos*5));
+	var incr = Math.round(pwd.length / 2);
+	var modu = Math.pow(2, 31) - 1;
+	var salt = parseInt(str.substring(str.length - 8, str.length), 16);
+	str = str.substring(0, str.length - 8);
+	prand += salt;
+	while(prand.length > 10){
+		prand = (parseInt(prand.substring(0, 10)) + parseInt(prand.substring(10, prand.length))).toString();
+	}
+	prand = (mult * prand + incr) % modu;
+	var enc_chr = "";
+	var enc_str = "";
+	for(var i=0; i<str.length; i+=2){
+		enc_chr = parseInt(parseInt(str.substring(i, i+2), 16) ^ Math.floor((prand / modu) * 255));
+		enc_str += String.fromCharCode(enc_chr);
+	prand = (mult * prand + incr) % modu;
+	}
+	if(!enc_str.match(/\.html/g)){
+		alert("비밀번호가 일치하지 않습니다");
+	}
+	else {
+		window.location.href=enc_str;
+	}
 }
+
 
 function darkDayHandler(self) {
     var target = document.querySelector('body');
